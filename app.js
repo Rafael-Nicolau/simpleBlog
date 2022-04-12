45745e2fcc06d50d927de48d2a425d9482631847
Rafael Nicolau <nicolaurafael@hotmail.com>
2022-04-12 16:01:11 +0200

    add mongoDB support with del funciton

--- app.js
+++ app.js
@@ -1,50 +1,74 @@
 //jshint esversion:6
 
+
+//First, lets config a lot of dependencies
 const express = require("express");
+const dotenv = require('dotenv').config();
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const { param } = require("express/lib/router");
 const { render } = require("ejs");
 const { renderFile } = require("ejs");
-//const { find, findKey, includes } = require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
 
+//const { find, findKey, includes } = require("lodash");
+const my_api = process.env.MY_APIKEY;
+
+//Now some dummy content
+const homeStartingContent = "Welcome to this simple Journal, it is made using MongoDB, Express, EJS and Node.";
+const aboutContent = "Made for simple tech knowledge demonstration, it is made using MongoDB, Express, EJS and Node.";
+const contactContent = "You can contact me:";
+
+//Here we use EJS, so let's make the request
 app.set('view engine', 'ejs');
 
+//Vintage bodyparser, lol
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//this is the "blog database"
-const posts = [];
+//Lets fire up the mongoDB Atlas
+mongoose.connect("mongodb+srv://"+my_api+"@cluster0.laqkg.mongodb.net/simpleBlog?retryWrites=true&w=majority");
 
+//Create the postSchema
+const postSchema ={
+  title: String,
+  content: String
+};
+
+//Mongoose model
+const Post = mongoose.model("Post", postSchema); //So we are telling mongoose, using loadash to create a collection called Posts on the DB, inside simpleBlog
+
+//tell express what to do when home page is accessed
 app.get('/', (req, res) => {
-  res.render("home",{
-    homeStartingContent: homeStartingContent,
-    posts: posts
+
+  Post.find({}, function(err, foundItems){
+    //search Posts collection (remember, lodash automatic puts on plural, if an err return log the error, if no errors, give the posts as json)
+    err ? console.log(err) : res.render("home",{
+      homeStartingContent: homeStartingContent,
+      posts: foundItems
+    });
   });
 });
 
-//express route with custom parameter
+//express route with custom parameter for the custom blog post
 app.get('/posts/:blogPost', (req, res) => {
 
-  const requestedTitle = _.lowerCase(req.params.blogPost);
+  Post.findOne({_id: req.params.blogPost}, function(err, foundItems){
+    err ? console.log(err) : res.render("post", {
+      title: foundItems.title,
+      post: foundItems.content,
+      id: foundItems._id
+    });
+  });
+});
 
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title);
-    
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        post: post.post
-      });
-    } else {
-      console.log("nop");
-    }
+app.post('/delete', function (req, res) {
+
+  const removePost = req.body.id;
+  Post.findByIdAndDelete(removePost, function(err){
+    err?console.log(err):res.redirect("/");
   });
 });
 
@@ -68,16 +92,22 @@ app.get('/compose', (req, res) => {
 
 app.post('/compose', function (req, res) {
 
-  const post = {
+  const post = new Post ({
       title: req.body.blogTitle,
-      post: req.body.blogPost
-  };
+      content: req.body.blogPost
+  });
 
-  posts.push(post);
+  post.save(function(err){
+    if (!err){
+      res.redirect("/");
+    } else {
+      console.log(err);
+    }
+  });
 
-  res.redirect("/");
+  
 });
 
-app.listen(3000, function() {
+app.listen(process.env.port || 3000, function() {
   console.log("Server started on port 3000");
 });
